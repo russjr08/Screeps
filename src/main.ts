adeaab2452a51273d98213cd68932234eaf0480c
Russell Richardson <Russell@KronosAD.com>
2019-11-12 23:56:48 -0500

    Basic screep setup - harvesters upgrade controller

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { ErrorMapper } from "utils/ErrorMapper";
+import harvestRole from './roles/harvester';
+
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
@@ -11,4 +13,30 @@ export const loop = ErrorMapper.wrapLoop(() => {
       delete Memory.creeps[name];
     }
   }
+
+  const MAIN_SPAWN = "Spawn1";
+  const MIN_HARVESTERS = 3;
+  const MIN_UPGRADERS = 1;
+
+  // Check to make sure we have the minimum amount of each creep role, and if not, spawn more.
+  var harvesters = _(Game.creeps).filter((c: Creep) => c.memory.role == 'harvester');
+
+  if (harvesters.size() < MIN_HARVESTERS) {
+    var status = Game.spawns[MAIN_SPAWN].spawnCreep([WORK, WORK, MOVE, CARRY], createRandomRoleName('Harvester'), { memory: { role: 'harvester' } });
+    if (status == OK) {
+      console.log('Spawned new harvester creep');
+    }
+  }
+
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester') {
+      harvestRole.run(creep);
+    }
+  }
+
 });
+
+function createRandomRoleName(role: string): string {
+  return role + '_' + (Math.floor(Math.random() * 65534) + 1)
+}
---
+++ src/roles/role.ts
@@ -0,0 +1,3 @@
+export default interface Role {
+    run(creep: Creep): void
+}
